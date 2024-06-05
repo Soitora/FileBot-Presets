445849ad358643d496c27eeb4000fa5615baa59a
Soitora <simon.mattila@protonmail.com>
2024-06-05 19:15:18 +0200

    Revert "Add ellipses even if truncated is below 50 chars"
    This reverts commit 34103c7680f057701f57563c340ef7c43dd12ef9.

--- Presets/Series/Episode.groovy
+++ Presets/Series/Episode.groovy
@@ -3,12 +3,11 @@
 {sxe.pad(2)}
 {
     // Check if the title matches certain patterns
-    if (t =~ /(Volume|Episode).[0-9]+/) {
-        null
-    } else {
-        // Replace colons with a special character, validate the filename, truncate properly, and ensure it ends appropriately
-        def truncated = t.replaceAll(":", "꞉").validateFileName().take(50).replaceAll(/(\s\w+)?$/, "").replaceAll(/\W*$/, "").replaceAll(/꞉$/, "")
-        def isTruncated = truncated.length() < t.length() || truncated.length() == 50
-        " - " + truncated + (isTruncated ? "..." : "")
-    }
+    t ==~ /(Volume|Episode).[0-9]+/ ? null :
+    // Replace colons with a special character, validate the filename, truncate properly, and ensure it ends appropriately
+    " - " + t.replaceAll(":", "꞉").validateFileName().take(50).replaceAll(/(\s\w+)?$/, "").replaceAll(/\W*$/, "").replaceAll(/꞉$/, "")
+}
+{
+    // Add ellipsis if the title was truncated
+    if (t.length() > 50) "..."
 }
